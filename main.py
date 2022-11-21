6e70e6f4efa262d324f8dc581ba51f8f6e4bc816
yehudafreedman <hudafreedman@gmail.com>
2022-11-21 16:31:36 +0200

    balls

---
+++ main.py
@@ -0,0 +1,153 @@
+# https://www.101computing.net/getting-started-with-pygame/
+# base brickbreaker game source
+# fork and go nuts
+
+
+import pygame
+
+from paddle import Paddle
+from ball import Ball
+from brick import Brick
+
+pygame.init()
+
+# theme
+WHITE = (255, 255, 255)
+DARKBLUE = (36, 90, 190)
+LIGHTBLUE = (0, 176, 240)
+RED = (255, 0, 0)
+ORANGE = (255, 100, 0)
+YELLOW = (255, 255, 0)
+
+score = 0
+lives = 3
+carryOn = True
+clock = pygame.time.Clock()
+
+# window settings
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("BrickBreaker")
+
+all_sprites_list = pygame.sprite.Group()
+
+# create the paddle sprite
+paddle = Paddle(LIGHTBLUE, 100, 10)
+paddle.rect.x = 350
+paddle.rect.y = 560
+
+# Create the ball sprite
+ball = Ball(WHITE, 10, 10)
+ball.rect.x = 345
+ball.rect.y = 195
+
+all_bricks = pygame.sprite.Group()
+for i in range(7):
+    brick = Brick(RED, 80, 30)
+    brick.rect.x = 60 + i * 100
+    brick.rect.y = 60
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
+for i in range(7):
+    brick = Brick(ORANGE, 80, 30)
+    brick.rect.x = 60 + i * 100
+    brick.rect.y = 100
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
+for i in range(7):
+    brick = Brick(YELLOW, 80, 30)
+    brick.rect.x = 60 + i * 100
+    brick.rect.y = 140
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
+
+all_sprites_list.add(ball)
+all_sprites_list.add(paddle)
+
+
+# main game loop
+while carryOn:
+    # main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            carryOn = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                carryOn = False
+
+    # Moving the paddle when the use uses the arrow keys
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        paddle.moveLeft(10)
+    if keys[pygame.K_RIGHT]:
+        paddle.moveRight(10)
+
+    # game logic
+    all_sprites_list.update()
+
+    # Check if the ball is bouncing against any of the 4 walls:
+    if ball.rect.x >= 790:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.x <= 0:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.y > 590:
+        ball.velocity[1] = -ball.velocity[1]
+        lives -= 1
+        if lives == 0:
+            # Display Game Over Message for 3 seconds
+            font = pygame.font.Font(None, 74)
+            text = font.render("GAME OVER", 1, WHITE)
+            screen.blit(text, (250, 300))
+            pygame.display.flip()
+            pygame.time.wait(3000)
+
+            # Stop the Game
+            carryOn = False
+
+    if ball.rect.y < 40:
+        ball.velocity[1] = -ball.velocity[1]
+
+    # Detect collisions between the ball and the paddles
+    if pygame.sprite.collide_mask(ball, paddle):
+        ball.rect.x -= ball.velocity[0]
+        ball.rect.y -= ball.velocity[1]
+        ball.bounce()
+
+    # Check if there is the ball collides with any of bricks
+    brick_collision_list = pygame.sprite.spritecollide(ball, all_bricks, False)
+    for brick in brick_collision_list:
+        ball.bounce()
+        score += 1
+        brick.kill()
+        if len(all_bricks) == 0:
+            # Display Level Complete Message for 3 seconds
+            font = pygame.font.Font(None, 74)
+            text = font.render("LEVEL COMPLETE", 1, WHITE)
+            screen.blit(text, (200, 300))
+            pygame.display.flip()
+            pygame.time.wait(3000)
+
+            # Stop the Game
+            carryOn = False
+
+    # draw logic
+    screen.fill(DARKBLUE)
+    pygame.draw.line(screen, WHITE, [0, 38], [800, 38], 2)
+
+    font = pygame.font.Font(None, 34)
+    text = font.render("Score: " + str(score), 1, WHITE)
+    screen.blit(text, (20, 10))
+    text = font.render("Lives: " + str(lives), 1, WHITE)
+    screen.blit(text, (650, 10))
+
+    # draw all sprites
+    all_sprites_list.draw(screen)
+
+    # update screen
+    pygame.display.flip()
+
+    # limit to 60 fps
+    clock.tick(60)
+
+
+pygame.quit()
